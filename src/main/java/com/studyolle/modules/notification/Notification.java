54dc36d7dfafb75a15e874a25483bb38a1b7fd3a
cocodori <moradulee@gmail.com>
2020-12-30 18:26:28 +0900

    Notification Domain

---
+++ src/main/java/com/studyolle/modules/notification/Notification.java
@@ -0,0 +1,28 @@
+package com.studyolle.modules.notification;
+
+import com.studyolle.modules.account.Account;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter @Setter @EqualsAndHashCode(of = "id")
+@Entity
+public class Notification {
+    @Id @GeneratedValue
+    private Long id;
+    private String title;
+    private String link;
+    private String message;
+    private boolean checked;
+
+    @ManyToOne
+    private Account account;
+
+    private LocalDateTime createdLocalDateTime;
+
+    @Enumerated(EnumType.STRING)
+    private NotificationType notificationType;
+}
