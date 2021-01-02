8c3dd52f30c07222c1feffb42ba962195ae7af47
cocodori <moradulee@gmail.com>
2021-01-02 21:05:02 +0900

    Notification about Event

--- src/main/java/com/studyolle/modules/event/EventService.java
+++ src/main/java/com/studyolle/modules/event/EventService.java
@@ -1,9 +1,12 @@
 package com.studyolle.modules.event;
 
 import com.studyolle.modules.account.Account;
+import com.studyolle.modules.event.event.EnrollmentAcceptedEvent;
 import com.studyolle.modules.study.Study;
+import com.studyolle.modules.study.event.StudyUpdateEvent;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,11 +19,14 @@ public class EventService {
     private final EventRepository eventRepository;
     private final ModelMapper modelMapper;
     private final EnrollmentRepository enrollmentRepository;
+    private final ApplicationEventPublisher eventPublisher;
 
     public Event createEvent(Event event, Study study, Account account) {
         event.setCreatedBy(account);
         event.setCreateDateTime(LocalDateTime.now());
         event.setStudy(study);
+        eventPublisher.publishEvent(new StudyUpdateEvent(event.getStudy()
+                , " '"+event.getTitle()+"' 모임을 만들었습니다."));
 
         return eventRepository.save(event);
     }
@@ -29,15 +35,21 @@ public class EventService {
     public void updateEvent(Event event, EventForm eventForm) {
         modelMapper.map(eventForm, event);
         event.acceptWaitingList();
+        eventPublisher.publishEvent(new StudyUpdateEvent(event.getStudy(),
+                " '"+event.getTitle()+"' 모임에 새 소식이 있습니다."));
     }
 
     public void deleteEvent(Event event) {
         eventRepository.delete(event);
+        eventPublisher.publishEvent(new StudyUpdateEvent(event.getStudy(),
+                " '"+event.getTitle()+"' 모임을 취소했습니다."));
     }
 
     public void newEnrollment(Event event, Account account) {
         if (!enrollmentRepository.existsByEventAndAccount(event, account)) {
-            Enrollment enrollment= new Enrollment();
+            Enrollment enrollment= new Enrollment
+
+                    ();
             enrollment.setEnrolledAt(LocalDateTime.now());
             enrollment.setAccepted(event.isAbleToAcceptWaitingEnrollment());
             enrollment.setAccount(account);
@@ -57,10 +69,12 @@ public class EventService {
 
     public void acceptEnrollment(Event event, Enrollment enrollment) {
         event.accept(enrollment);
+        eventPublisher.publishEvent(new EnrollmentAcceptedEvent(enrollment));
     }
 
     public void rejectEnrollment(Event event, Enrollment enrollment) {
         event.reject(enrollment);
+        eventPublisher.publishEvent(new EnrollmentAcceptedEvent(enrollment));
     }
 
     public void checkInEnrollment(Enrollment enrollment) {
